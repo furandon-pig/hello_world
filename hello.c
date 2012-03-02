e2abf7ffb2d0335b5af4c6acd32a8518b999c75a
furandon_pig <furandon.pig@gmail.com>
2012-03-03 04:23:33 +0900

    Added hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ * hello.c
+ *
+ * Compile:
+ *   gcc -Wall -Werror -g -o hello hello.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello,world.\n");
+
+	exit(EXIT_SUCCESS);
+}
