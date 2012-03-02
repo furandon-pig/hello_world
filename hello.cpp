b05906c211cb3a334000609a6bdcb9654bb5fea1
furandon_pig <furandon.pig@gmail.com>
2012-03-03 04:41:00 +0900

    Added hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * hello.cpp
+ *
+ * Compile:
+ *   g++ -Wall -Werror -g -o hello hello.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << "hello,world." << endl;
+
+	return 0;
+}
